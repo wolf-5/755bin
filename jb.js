fb32c19f8fd97681c9aa7fddbe34eb1a1b59c9c0
thewolfgames <thewolfgames@yahoo.com>
2021-09-13 21:40:59 +0300

    Update jb.js

--- jb.js
+++ jb.js
@@ -142975,8 +142975,8 @@ var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 if(main_ret == 179 || main_ret == 0){
-	document.getElementById("progress").innerHTML="Jailbreak Complete.. Now Run Payloads!!";
+	document.getElementById("progress").innerHTML="PS4 Jailbreak 7.5X Jailbreak Complete !!";
 } else{
 	alert("Jailbreak Failed!! Please Restart and Start Fresh!!!");
-	document.getElementById("progress").innerHTML="Jailbreak Failed, Restart and Start Fresh !!";
-}
+	document.getElementById("progress").innerHTML="PS4 Jailbreak 7.5X Jailbreak Failed, Restart and Start Fresh !!";
+} 
